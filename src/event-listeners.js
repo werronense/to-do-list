d4f0c3519127dd41830a6f975377ada8c92715fd
Stephen Werronen <stephen.werronen@gmail.com>
2020-07-08 11:35:00 -0700

    Refactor event listeners

--- src/event-listeners.js
+++ src/event-listeners.js
@@ -1,5 +1,26 @@
 import { toggleProjectForm, toggleTodoForm } from './toggle.js';
 
+
+function setProjectSubmitEventListener() {
+  const projectForm = document.querySelector('.project-form');
+
+  projectForm.addEventListener('submit', e => {
+    e.preventDefault();
+    toggleProjectForm();
+  });
+}
+
+
+function setTodoSubmitEventListener() {
+  const todoForm = document.querySelector('.todo-form');
+
+  todoForm.addEventListener('click', e => {
+    e.preventDefault();
+    toggleTodoForm();
+  });
+}
+
+
 function setEventListeners() {
   const projectButton = document.querySelector('.project-button');
   const todoButton = document.querySelector('.todo-button');
@@ -7,22 +28,9 @@ function setEventListeners() {
   projectButton.addEventListener('click', toggleProjectForm);
   todoButton.addEventListener('click', toggleTodoForm);
 
-  const projectSubmit = document.querySelector(
-    '.project-form input[type="submit"]'
-  );
-  const todoSubmit = document.querySelector(
-    '.todo-form input[type="submit"]'
-  );
-
-  projectSubmit.addEventListener('click', e => {
-    e.preventDefault();
-    toggleProjectForm();
-  });
-
-  todoSubmit.addEventListener('click', e => {
-    e.preventDefault();
-    toggleTodoForm();
-  });
+  setProjectSubmitEventListener();
+  setTodoSubmitEventListener();
 }
+
 
 export { setEventListeners };
