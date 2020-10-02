a95d0cdce29d8104bebe04ee14f5e29c94586997
Stephen Werronen <stephen.werronen@gmail.com>
2020-10-02 08:49:53 -0700

    Update list styles and layout

--- src/populate-list.js
+++ src/populate-list.js
@@ -7,36 +7,37 @@ function populateList() {
 
   list.innerHTML = '';
 
-  heading.textContent = data.current.slice(0,1).toUpperCase() +
-    data.current.slice(1);
+  heading.innerHTML = `Current List: <b>
+    ${data.current.slice(0,1).toUpperCase() + data.current.slice(1)}</b>`;
 
-  todos.forEach(todo => {
+  todos.forEach((todo, i) => {
     const li = document.createElement('li');
     const p1 = document.createElement('p');
     const p2 = document.createElement('p');
     const button = document.createElement('button');
-    const span = document.createElement('span');
 
     p1.textContent =
       `${todo.title} -- due: ${todo.dueDate} -- priority: ${todo.priority}`;
 
     button.classList.add('description')
     button.textContent = "show description";
+
+    p2.classList.add(`item-${i}-details`, 'hidden');
+    p2.textContent = todo.description;
+
     button.addEventListener('click', e => {
       if (e.target.textContent.includes('show')) {
-        e.target.textContent = "hide description\t";
-        e.target.nextSibling.textContent = todo.description;
+        e.target.textContent = "hide description";
+        p2.classList.toggle('hidden');
       } else {
         e.target.textContent = "show description";
-        e.target.nextSibling.textContent = '';
+        p2.classList.toggle('hidden');
       }
     });
 
     li.appendChild(p1);
-
-    p2.appendChild(button);
-    p2.appendChild(span);
     li.appendChild(p2);
+    li.appendChild(button);
 
     list.appendChild(li);
   });
