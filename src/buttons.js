a65f834c6258bd366e95f29fc2b0e2432aa8ba92
Stephen Werronen <stephen.werronen@gmail.com>
2020-10-09 13:44:40 -0700

    Set event listener on edit buttons

--- src/buttons.js
+++ src/buttons.js
@@ -3,7 +3,17 @@ function createEditButton() {
   button.classList.add('button', 'small-button', 'todo-edit-button');
   button.textContent = 'edit';
 
-  // TODO: add the event listener
+  button.addEventListener('click', () => {
+    const projectForm = document.querySelector('.project-form');
+    const todoForm = document.querySelector('.todo-form');
+    const createButton = document.querySelector('.create-todo-button');
+    const changeButton = document.querySelector('.change-todo-button');
+
+    projectForm.classList.add('hidden');
+    createButton.classList.add('hidden');
+    changeButton.classList.remove('hidden');
+    todoForm.classList.remove('hidden');
+  });
 
   return button;
 }
