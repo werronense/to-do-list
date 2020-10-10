c1bb339ce4dcc7fe39a0060a89d910d3c9430dd5
Stephen Werronen <stephen.werronen@gmail.com>
2020-10-10 12:18:27 -0700

    Correct display of form buttons

--- src/toggle.js
+++ src/toggle.js
@@ -15,6 +15,11 @@ function toggleProjectForm() {
 function toggleTodoForm() {
   const todoForm = document.querySelector('.todo-form');
   const projectForm = document.querySelector('.project-form');
+  const createTodoButton = document.querySelector('.create-todo-button');
+  const changeTodoButton = document.querySelector('.change-todo-button');
+
+  changeTodoButton.classList.add('hidden');
+  createTodoButton.classList.remove('hidden');
 
   todoForm.classList.toggle('hidden');
   if (!Array.from(projectForm.classList).includes('hidden')) {
